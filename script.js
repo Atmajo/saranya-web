b5549ce64d1496e994910be1cd1308bc7f77e2cb
Atmajo <39727009+Atmajo@users.noreply.github.com>
2023-11-20 23:45:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-<!-- index.html -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Innovative Survey Site</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Innovative Survey</h1>
-        <form id="surveyForm">
-            <label for="question1">Question 1: What is your favorite color?</label>
-            <input type="text" id="question1" name="question1" required>
-
-            <label for="question2">Question 2: What is your favourite dish?</label>
-            <input type="text" id="question2" name="question2" required>
-
-            <label for="question2">Question 3: What is your favourite Bird?</label>
-            <input type="text" id="question3" name="question3" required>
-
-            <label for="question2">Question 4: How would you rate our service (1-10)?</label>
-            <input type="number" id="question4" name="question4" max="10" min="1" required>
-
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-
-    <script src="script.js"></script>
-</body>
-</html>
+// script.js
+function submitSurvey() {
+    const question1 = document.getElementById('question1').value;
+    const question2 = document.getElementById('question2').value;
+    const question3 = document.getElementById('question3').value;
+    const question4 = document.getElementById('question4').value
+    console.log("Heelo")
+    // You can perform actions with the survey data here (e.g., display an alert)
+    
+    setTimeout(()=> {
+        alert(`Survey submitted successfully!\nQuestion 1: ${question1}\nQuestion 2: ${question2}\nQuestion 3: ${question3}\nQuestion 4: ${question4}`);
+    },3000);
+    // Optionally, reset the form
+    document.getElementById('surveyForm').reset();
+}
