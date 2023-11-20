2f686df34fa63e08bd98d346a19bb9ffd5337f81
Atmajo <39727009+Atmajo@users.noreply.github.com>
2023-11-20 23:44:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-// script.js
-function submitSurvey() {
-    const question1 = document.getElementById('question1').value;
-    const question2 = document.getElementById('question2').value;
+<!-- index.html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Innovative Survey Site</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Innovative Survey</h1>
+        <form id="surveyForm">
+            <label for="question1">Question 1: What is your favorite color?</label>
+            <input type="text" id="question1" name="question1" required>
 
-    // You can perform actions with the survey data here (e.g., display an alert)
-    alert(`Survey submitted successfully!\nQuestion 1: ${question1}\nQuestion 2: ${question2}`);
+            <label for="question2">Question 2: What is your favourite dish?</label>
+            <input type="text" id="question2" name="question2" required>
 
-    // Optionally, reset the form
-    document.getElementById('surveyForm').reset();
-}
+            <label for="question2">Question 3: What is your favourite Bird?</label>
+            <input type="text" id="question3" name="question3" required>
+
+            <label for="question2">Question 4: How would you rate our service (1-10)?</label>
+            <input type="number" id="question4" name="question4" max="10" min="1" required>
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
